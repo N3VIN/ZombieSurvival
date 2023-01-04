0ac403dcc18fc4c7faeaacd1ec6a210c826ea5d5
N3VIN <n3vin19@gmail.com>
2023-01-04 15:28:30 +0100

    Initial Push

---
+++ ZombieGame/project/Behaviors.h
@@ -0,0 +1,36 @@
+/*=============================================================================*/
+// Copyright 2020-2021 Elite Engine
+/*=============================================================================*/
+// Behaviors.h: Implementation of certain reusable behaviors for the BT version of the Agario Game
+/*=============================================================================*/
+#ifndef ELITE_APPLICATION_BEHAVIOR_TREE_BEHAVIORS
+#define ELITE_APPLICATION_BEHAVIOR_TREE_BEHAVIORS
+//-----------------------------------------------------------------
+// Includes & Forward Declarations
+//-----------------------------------------------------------------
+#include "../inc/EliteMath/EMath.h"
+#include "IExamPlugin.h"
+#include "IExamInterface.h"
+#include "Exam_HelperStructs.h"
+#include "EBehaviorTree.h"
+#include "Timer.h"
+
+
+//-----------------------------------------------------------------
+// Behaviors
+//-----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
